6dfa7131069ef8f1eafa25a1582151c159f61775
thomas-young-2013 <1225646303@qq.com>
2017-03-08 22:45:04 +0800

    add thrift file

---
+++ common/src/main/resource/thrift/ParameterServerService.thrift
@@ -0,0 +1,26 @@
+namespace java com.thomas
+
+service ParameterServerService {
+  list<double> readRows(
+    1:string tableIdentifier,
+    2:i32 rowId,
+    3:i32 extra
+  ),
+  
+  void updateRows(
+    1:string tableIdentifier,
+    2:i32 rowId,
+    3:list<double> values
+  ),
+
+  bool round(
+    1:string tableIdentifier
+  ),
+
+  bool createTable(
+    1:string tableId,
+    2:list<string> machines,
+    3:list<double> initialVal
+  )
+}
+
